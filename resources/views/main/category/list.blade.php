ff515a3afedf948c49669ce01af5a4884ccd7f71
azaibMeer <azaibmir1292@gmail.com>
2024-06-25 15:52:42 +0500

    category work

--- resources/views/main/category/list.blade.php
+++ resources/views/main/category/list.blade.php
@@ -4,14 +4,15 @@
 <h1 class="subheader-title">
     Category
 </h1>
+<button class="btn btn-primary waves-effect waves-themed">Add Category</button>
 </div>
 <div class="row">
 <div class="col-xl-12">
     <div id="panel-1" class="panel">
         <div class="panel-container show">
             <div class="panel-content">
-                <table id="dt-basic-example" class="table table-bordered  w-100">
-                    <thead class="">
+                <table id="dt-basic-example" class="table table-hover table-bordered  w-100">
+                    <thead class="bg-primary-600">
                         <tr>
                             <th>Category</th>
                             <th>Status</th>
@@ -24,14 +25,14 @@
                             <td>{{ucwords($category->name)}}</td>
                             <td>{{$category->status == 1 ? 'Active' : 'Inactive'}}</td>
                             <td>
-                            <a href="javascript:void(0);" class="btn btn-primary btn-sm btn-icon waves-effect waves-themed">
+                            <a href="{{ url('category/edit', ['category' => $category->id]) }}" class="btn btn-primary btn-sm btn-icon waves-effect waves-themed">
                             <i class="fal fa-edit"></i>
                             </a>
-                            <a href="javascript:void(0);" class="btn btn-primary btn-sm btn-icon waves-effect waves-themed">
+                            <a href="{{ url('category/delete', ['category' => $category->id]) }}" class="btn btn-primary btn-sm btn-icon waves-effect waves-themed">
                             <i class="fal fa-trash"></i>
                             </a>
                             <a href="javascript:void(0);" class="btn btn-primary btn-sm btn-icon waves-effect waves-themed">
-                            <i class="fal fa-edit"></i>
+                            <i class="fal fa-circle"></i>
                             </a>
                             </td>
                         </tr>
